dafd1af9a4b97e4fa533252760af9ae468eec082
Bhagawanta <60783765+Bhagawanta@users.noreply.github.com>
2021-06-07 19:54:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import Sidebar from './components/Sidebar';
 import { BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import Overview from './pages/Overview';
-import Item from './pages/overview/Item'
+import Item from './pages/overview/Item';
+import Email from './pages/overview/Email';
 import { Reports, ReportsOne, ReportsTwo} from './pages/Reports';
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
    <Switch>
     <Route path='/overview' exact component={Overview}/>
     <Route path='/overview/item' exact component={Item}/>
+    <Route path='/overview/email' exact component={Email}/>
     <Route path='/reports' exact component={Reports}/>
     <Route path='/reports/reports2' exact component={ReportsOne}/>
     <Route path='/reports/reports3' exact component={ReportsTwo}/>
